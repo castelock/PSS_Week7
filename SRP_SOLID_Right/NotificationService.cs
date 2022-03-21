d23ad273dccc92025e690b45cee10646083c95ac
juan jesus ojeda castelo <joc028@ual.es>
2022-03-21 22:46:55 +0100

    Creation of the projects related to the SR Principle SOLID.

---
+++ SRP_SOLID_Right/NotificationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP_SOLID_Right
+{
+    public class NotificationService
+    {
+        private readonly SmtpClient _smtpClient;
+
+        public NotificationService(SmtpClient smtpClient)
+        {
+            _smtpClient = smtpClient;
+        }
+
+        public async Task SendEmail(MailMessage message)
+        {
+            await _smtpClient.SendMailAsync(message);
+        }
+    }
+}
---
+++ SRP_SOLID_Right/OrderService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP_SOLID_Right
+{
+    public class OrderService
+    {
+        private readonly NotificationService _notificationService;
+
+        public OrderService(NotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        public async Task Add(Order order)
+        {
+            // 01. Código para crear la orden
+
+            // 02. Notificar al cliente
+            var message = new MailMessage(new MailAddress("sales@admin.com"), (MailAddress)order.ClientEmail)
+            {
+                Subject = "Se le asignó una compra",
+                Body = "Estimado,\n Hemos creado su nueva orden de compra .."
+            };
+
+            await this._notificationService.SendEmail(message);
+        }
+    }
+
+    public class Order
+    {
+        public object ClientEmail { get; internal set; }
+    }
+}
