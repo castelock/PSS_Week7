7aafa5a5194ce1acf3d2858d7b7b937cb6626ec6
juan jesus ojeda castelo <joc028@ual.es>
2022-03-21 23:11:50 +0100

    Creation of the project OCP_SOLID_Wrong.

---
+++ OCP_SOLID_Wrong/NotificationService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP_SOLID_Wrong
+{
+    public class NotificationService
+    {
+        public async Task Send(List<Notification> notifications)
+        {
+            foreach (var notification in notifications)
+            {
+                if (notification.Type.Equals("sms"))
+                {
+                    await SendbySMS(notification.PhoneNumber, notification.Subject);
+                }
+
+                if (notification.Type.Equals("email"))
+                {
+                    await SendbyEmail(notification.Email, notification.Subject);
+                }
+
+                //If we want to add sending via WhatsApp or another channel in the near future,
+                //we will have to add an additional if.
+
+                if (notification.Type.Equals("whatsapp"))
+                {
+                    await SendbyWhatsApp(notification.PhoneNumber, notification.Subject);
+                }
+
+                if (notification.Type.Equals("slack"))
+                {
+                    await SendbySlack(notification.Channel, notification.Subject);
+                }
+
+                if (notification.Type.Equals("telegram"))
+                {
+                    await SendbyTelegram(notification.Channel, notification.Subject);
+                }
+            }
+        }
+
+        private Task SendbyTelegram(object channel, string subject)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task SendbySlack(object channel, string subject)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task SendbyWhatsApp(string phoneNumber, string subject)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task SendbySMS(string phoneNumber, string subject)
+        {
+            // Code to send a SMS
+        }
+
+        private async Task SendbyEmail(string to, string subject)
+        {
+            // Code to send an email
+        }
+    }
+
+    public class Notification
+    {
+        public object Type { get; internal set; }
+        public string PhoneNumber { get; internal set; }
+        public string Subject { get; internal set; }
+        public string Email { get; internal set; }
+        public object Channel { get; internal set; }
+    }
+}
---
+++ OCP_SOLID_Wrong/Program.cs
@@ -0,0 +1,18 @@
+﻿using OCP_SOLID_Wrong;
+
+var email = new Notification
+{
+    Type = "email",
+    Email = "customer@email.com",
+    Subject = "El asunto del correo"
+};
+
+var sms = new Notification
+{
+    Type = "sms",
+    PhoneNumber = "+051199999999",
+    Subject = "El mensaje del texto"
+};
+
+var notificationService = new NotificationService();
+await notificationService.Send(new List<Notification> { email, sms });
