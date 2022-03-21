9f8dda540e8ca883b0f4ad204eab24541d2517e2
juan jesus ojeda castelo <joc028@ual.es>
2022-03-21 22:35:18 +0100

    Creation of the project Binding.

---
+++ Binding/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using Binding;
+
+Console.WriteLine("The options for this program are: {0} 1- Early binding. {0} " +
+                "2- Late binding. {0} ", Environment.NewLine);
+Console.WriteLine("Insert one of the previous options: ");
+String option = Console.ReadLine();
+
+
+switch (option)
+{
+    case "1":
+        // creating object of Student class
+        Student g = new Student();
+
+        // Calling the method of Student class
+        g.details("Ankita", "C#");
+
+        // Calling "mymethod()" gives error
+        // because this method does not 
+        // belong to class Student or compiler 
+        // does not know mymethod() at compile time
+        //g.mymethod();
+        break;
+    case "2":
+        // Dynamic objects
+        dynamic obj = 4;
+        dynamic obj1 = 5.678;
+
+        // Display the type of objects
+        Console.WriteLine("The type of the objects are :");
+
+        // GetType() method is 
+        // used to get the type
+        Console.WriteLine(obj.GetType());
+        Console.WriteLine(obj1.GetType());
+        break;
+    default:
+        Console.WriteLine("The option is not valid.");
+        break;
+}
---
+++ Binding/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binding
+{
+    public class Student
+    {
+        // data members
+        public string name;
+        public string subject;
+
+        // public method
+        public void details(string name, string subject)
+        {
+            this.name = name;
+            this.subject = subject;
+            Console.WriteLine("Myself: " + name);
+            Console.WriteLine("My Favorite Subject is: " + subject);
+        }
+    }
+}
